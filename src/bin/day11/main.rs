fd28de31e311166e423d81ce7e8fae79ad8cc655
techno-sam <77073745+techno-sam@users.noreply.github.com>
2025-12-11 13:01:28 +0100

    Day 11 Part 2

--- src/bin/day11/main.rs
+++ src/bin/day11/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fmt::Display, fs, ops::{Index, IndexMut, Mul}};
+use std::{collections::HashMap, fmt::Display, fs, ops::{Add, AddAssign, Index, IndexMut, Mul}};
 
 use nom::{character::complete, multi::separated_list1, sequence::separated_pair, IResult, Parser};
 use utils::{parse_complete, Point};
@@ -21,7 +21,26 @@ iii: out
 ".trim_matches('\n').to_owned()
 }
 
-const PART2: bool = false;
+#[allow(dead_code)]
+fn example2() -> String {
+    "
+svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out
+".trim_matches('\n').to_owned()
+}
+
+const PART2: bool = true;
 
 fn main() {
     println!("AOC 2025 Day 11");
@@ -67,7 +86,7 @@ fn test_p1() {
 #[test]
 fn test_p2() {
     if PART2 {
-        assert_eq!(part2(&example()), 42);
+        assert_eq!(part2(&example2()), 2);
     }
 }
 
@@ -232,12 +251,14 @@ impl Rack {
 fn part1(data: &str) -> usize {
     let mut rack = parse_complete(&mut Rack::parse, data);
 
-    println!("Original[{}x{}]:\n{}\n\n", rack.matrix0.n, rack.matrix0.n, rack.matrix0);
+    /*[bench exclude]*/ {
+        println!("Original[{}x{}]:\n{}\n\n", rack.matrix0.n, rack.matrix0.n, rack.matrix0);
+    }
 
     for i in 1usize.. {
         let all_zero = rack.step();
 
-        /* [bench exclude] */ {
+        /*[bench exclude]*/ {
             if cfg!(test) || i % 10 == 0 {
                 println!("Step {}:\n{}\n", i, rack.matrix);
             } else {
@@ -256,7 +277,142 @@ fn part1(data: &str) -> usize {
     rack.total_count
 }
 
+#[derive(Clone, Copy, Debug, Default)]
+struct PathCounter {
+    through_both: usize,
+    through_fft: usize,
+    through_dac: usize,
+    through_neither: usize
+}
+impl Add for PathCounter {
+    type Output = PathCounter;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        PathCounter {
+            through_both: self.through_both + rhs.through_both,
+            through_fft: self.through_fft + rhs.through_fft,
+            through_dac: self.through_dac + rhs.through_dac,
+            through_neither: self.through_neither + rhs.through_neither,
+        }
+    }
+}
+impl AddAssign for PathCounter {
+    fn add_assign(&mut self, rhs: Self) {
+        self.through_both += rhs.through_both;
+        self.through_fft += rhs.through_fft;
+        self.through_dac += rhs.through_dac;
+        self.through_neither += rhs.through_neither;
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Node {
+    connections: Vec<Label>,
+    /// the number of paths from here, to the target
+    counts: PathCounter,
+}
+impl Node {
+    fn new(connections: Vec<Label>) -> Self {
+        Self { connections, counts: PathCounter::default() }
+    }
+
+    fn out() -> Self {
+        Self {
+            connections: Vec::new(),
+            counts: PathCounter {
+                through_neither: 1,
+                ..PathCounter::default()
+            }
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+struct TopoRack {
+    nodes: HashMap<Label, Node>,
+    heights: Vec<(Label, usize)>,
+}
+impl TopoRack {
+    const SVR: Label = Label(['s', 'v', 'r']);
+    const OUT: Label = Label(['o', 'u', 't']);
+    const DAC: Label = Label(['d', 'a', 'c']);
+    const FFT: Label = Label(['f', 'f', 't']);
+
+    fn parse(input: &str) -> IResult<&str, Self> {
+        let (remainder, connections) = separated_list1(complete::line_ending, Rack::parse_connection).parse(input)?;
+        let mut nodes: HashMap<Label, Node> = connections.into_iter()
+            .map(|(key, labels)| (key, Node::new(labels)))
+            .collect();
+        nodes.insert(Self::OUT, Node::out());
+
+        let mut heights: HashMap<Label, usize> = HashMap::new();
+        heights.insert(Self::OUT, 0);
+
+        fn calculate_height(node: Label, nodes: &HashMap<Label, Node>, heights: &mut HashMap<Label, usize>) -> usize {
+            if let Some(&height) = heights.get(&node) {
+                height
+            } else {
+                let height = nodes.get(&node).unwrap_or_else(|| panic!("{} exists", node))
+                    .connections.iter()
+                    .map(|child| calculate_height(*child, nodes, heights) + 1)
+                    .max().unwrap_or(0);
+                heights.insert(node, height);
+                height
+            }
+        }
+        calculate_height(Self::SVR, &nodes, &mut heights);
+
+        let mut heights: Vec<(Label, usize)> = heights.into_iter().collect();
+        heights.sort_unstable_by_key(|(_, height)| *height);
+
+        Ok((remainder, Self { nodes, heights }))
+    }
+
+    fn print_heights(&self) {
+        for (lbl, height) in &self.heights {
+            println!("{} {}", lbl, height);
+        }
+    }
+
+    fn count(&mut self) -> usize {
+        for (lbl, _) in &self.heights {
+            let mut total = PathCounter::default();
+
+            for child in &self.nodes.get(lbl).unwrap().connections {
+                let child_counter = self.nodes.get(child).unwrap().counts;
+                total += child_counter;
+            }
+
+            match *lbl {
+                Self::DAC => {
+                    total.through_both += total.through_fft;
+                    total.through_fft = 0;
+
+                    total.through_dac += total.through_neither;
+                    total.through_neither = 0;
+                }
+                Self::FFT => {
+                    total.through_both += total.through_dac;
+                    total.through_dac = 0;
+
+                    total.through_fft += total.through_neither;
+                    total.through_neither = 0;
+                }
+                _ => {}
+            }
+
+            self.nodes.get_mut(lbl).unwrap().counts += total;
+        }
+
+        self.nodes.get(&Self::SVR).unwrap().counts.through_both
+    }
+}
+
 fn part2(data: &str) -> usize {
-    todo!();
+    let mut rack = parse_complete(&mut TopoRack::parse, data);
+    if cfg!(test) {
+        rack.print_heights();
+    }
+    rack.count()
 }
 
